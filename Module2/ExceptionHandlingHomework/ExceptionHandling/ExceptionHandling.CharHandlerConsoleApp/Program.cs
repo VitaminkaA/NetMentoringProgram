65c02498a1bee4d7e3a016fc3d183e47a83ff93b
Tatsiana Panasiuk <Tatsiana.Panasiuk@thomsonreuters.com>
2020-03-03 12:39:58 +0300

    Add a solution to the first task of the ExceptionHandling module.

--- Module2/ExceptionHandlingHomework/ExceptionHandling/ExceptionHandling.CharHandlerConsoleApp/Program.cs
+++ Module2/ExceptionHandlingHomework/ExceptionHandling/ExceptionHandling.CharHandlerConsoleApp/Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using System.Linq;
 
 namespace ExceptionHandling.CharHandlerConsoleApp
 {
     class Program
     {
+        private static volatile bool isRunning = true;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.CancelKeyPress += Cansel;
+
+            while (isRunning)
+            {
+                var line = Console.ReadLine();
+                try
+                {
+                    Console.WriteLine($"First character: '{line.First()}'");
+                }
+                catch (InvalidOperationException e)
+                {
+                    Console.WriteLine($"Exception: {e.Message}");
+                }
+            }
+        }
+
+        private static void Cansel(object sender, ConsoleCancelEventArgs e)
+        {
+            e.Cancel = true;
+            isRunning = false;
         }
     }
 }
