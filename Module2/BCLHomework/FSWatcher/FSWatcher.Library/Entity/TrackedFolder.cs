d19a93707dd61d3426536813a1aab33a3573468b
VitaminkaA <59922974+VitaminkaA@users.noreply.github.com>
2020-03-04 12:56:27 +0300

    HomeworkBCL. (#5)
    * Add a FSWatcher project.
    
    * Rename field
    
    * Typo correction.

---
+++ Module2/BCLHomework/FSWatcher/FSWatcher.Library/Entity/TrackedFolder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace FSWatcher.Library.Entity
+{
+    public class TrackedFolder
+    {
+        public string Path { get; set; }
+        public string DefaultMoveFolder { get; set; }
+        public bool IncludeNumbering { get; set; }
+        public bool IncludeMoveDate { get; set; }
+        public Dictionary<Regex, string> Templates { get; set; }
+        public int Counter { get; set; }
+    }
+}
---
+++ Module2/BCLHomework/FSWatcher/FSWatcher.Library/FolderWatcherService.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using FSWatcher.Library.Entity;
+using System.IO;
+using System.Linq;
+using System.Text;
+using FSWatcher.Library.Services;
+
+namespace FSWatcher.Library
+{
+    public class FolderWatcherService : IDisposable
+    {
+        private readonly IFolderWatcherProvider _folderWatcher;
+        private readonly IFileTransferService _fileTransferService;
+        private readonly TrackedFolder _folder;
+
+        public event Action<NewFileInfo> NewFileFound;
+        public event Action RuleFound, RuleNotFound, SuccessfulFileTransfer;
+
+        public FolderWatcherService(TrackedFolder folder)
+        {
+            _folder = folder ?? throw new ArgumentNullException();
+
+            _folderWatcher = new FolderWatcherProvider();
+            _folderWatcher.Created += NewFile;
+            _fileTransferService = new FileTransferService();
+        }
+
+        public void StartWatching() =>
+            _folderWatcher.StartWatching(_folder.Path
+                                         ?? throw new NullReferenceException());
+
+        private void NewFile(string fullPath)
+        {
+            if (fullPath == null)
+                throw new ArgumentNullException();
+
+            var newFileInfo = _fileTransferService.GetFileInfo(fullPath);
+
+            CallEvent(NewFileFound, newFileInfo);
+
+            var (regex, value) = _folder.Templates.FirstOrDefault(t
+                => t.Key.IsMatch(newFileInfo.Name));
+
+            CallEvent(regex == null ? RuleNotFound : RuleFound);
+
+            var destinationFolder = value ?? _folder.DefaultMoveFolder;
+
+            if (_folder.IncludeMoveDate)
+                newFileInfo.Name = $"({DateTimeOffset.Now.Date.ToShortDateString().Replace('/', '-')}){ newFileInfo.Name}";
+            if (_folder.IncludeNumbering)
+                newFileInfo.Name = $"({_folder.Counter++}){newFileInfo.Name}";
+            _fileTransferService.MoveFolder(fullPath, Path.Combine(destinationFolder, newFileInfo.Name));
+
+            CallEvent(SuccessfulFileTransfer);
+        }
+
+        public void Dispose()
+            => _folderWatcher.Dispose();
+
+        private static void CallEvent(Action action)
+            => action?.Invoke();
+
+        private static void CallEvent<T>(Action<T> action, T item1)
+            => action?.Invoke(item1);
+    }
+}
