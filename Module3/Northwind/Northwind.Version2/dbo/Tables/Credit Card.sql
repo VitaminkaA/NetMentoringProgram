f497f1942a4d4dbabc1ff35c710cd0fb12184bf4
Tatsiana Panasiuk <Tatsiana.Panasiuk@thomsonreuters.com>
2020-03-09 18:20:31 +0300

    Add a solution for task 3.

---
+++ Module3/Northwind/Northwind.Version2/dbo/Tables/Credit Card.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [Credit Cards](
+	[CardNumber] [int] IDENTITY(1,1) not null,
+	[ExpirationDate] [date] not null,
+	[CardHolderName] [nvarchar](150) not null,
+	[EmployeeID] [int] not null,
+	CONSTRAINT [PK_CardNumber] PRIMARY KEY (CardNumber),
+	CONSTRAINT [FK_EmployeeID] FOREIGN KEY (EmployeeID) REFERENCES [dbo].[Employees] (EmployeeID) ON DELETE CASCADE
+)
