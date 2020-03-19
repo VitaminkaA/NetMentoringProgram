cc1a9205d994acf6d34740067613940d1b0b1040
Tatsiana Panasiuk <Tatsiana.Panasiuk@thomsonreuters.com>
2020-03-19 14:15:58 +0300

    Add a AddOrder method.

--- Module4/Northwind/Northwind.DAL/OrderRepository.cs
+++ Module4/Northwind/Northwind.DAL/OrderRepository.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Data.Common;
 using System.Data.SqlClient;
 using Northwind.DAL.Mapping;
+using Northwind.DAL.Extensions;
 
 namespace Northwind.DAL
 {
@@ -22,6 +23,54 @@ namespace Northwind.DAL
                                ?? throw new ArgumentNullException());
             _connectionString = connectionString
                                 ?? throw new ArgumentNullException();
+        }
+
+        public void AddOrder(Order order)
+        {
+            using var connection = _providerFactory.CreateConnection();
+            connection.ConnectionString = _connectionString;
+            connection.Open();
+
+            var commandText =
+                "INSERT INTO [dbo].[Orders] VALUES(" +
+                $"@{nameof(order.CustomerID)}," +
+                $"@{nameof(order.EmployeeID)}," +
+                $"@{nameof(order.OrderDate)}," +
+                $"@{nameof(order.RequiredDate)}," +
+                $"@{nameof(order.ShippedDate)}," +
+                $"@{nameof(order.ShipVia)}," +
+                $"@{nameof(order.Freight)}," +
+                $"@{nameof(order.ShipName)}," +
+                $"@{nameof(order.ShipAddress)}," +
+                $"@{nameof(order.ShipCity)}," +
+                $"@{nameof(order.ShipRegion)}," +
+                $"@{nameof(order.ShipPostalCode)}," +
+                $"@{nameof(order.ShipCountry)}"
+                + ")";
+
+            using var command = connection.CreateCommand();
+            command.Parameters.AddRange(new[]
+            {
+                command.CreateParameter($"@{nameof(order.CustomerID)}", order.CustomerID),
+                command.CreateParameter($"@{nameof(order.EmployeeID)}", order.EmployeeID),
+                command.CreateParameter($"@{nameof(order.OrderDate)}", order.OrderDate.HasValue 
+                    ? $"{order.OrderDate.Value:yyyy - MM - dd HH: mm:ss}" : null),
+                command.CreateParameter($"@{nameof(order.RequiredDate)}", order.RequiredDate.HasValue 
+                    ? $"{order.RequiredDate.Value:yyyy - MM - dd HH: mm:ss}" : null),
+                command.CreateParameter($"@{nameof(order.ShippedDate)}", order.ShippedDate.HasValue 
+                    ? $"{order.ShippedDate.Value:yyyy - MM - dd HH: mm:ss}" : null),
+                command.CreateParameter($"@{nameof(order.ShipVia)}", order.ShipVia),
+                command.CreateParameter($"@{nameof(order.Freight)}", order.Freight),
+                command.CreateParameter($"@{nameof(order.ShipName)}", order.ShipName),
+                command.CreateParameter($"@{nameof(order.ShipAddress)}", order.ShipAddress),
+                command.CreateParameter($"@{nameof(order.ShipCity)}", order.ShipCity),
+                command.CreateParameter($"@{nameof(order.ShipRegion)}", order.ShipRegion),
+                command.CreateParameter($"@{nameof(order.ShipPostalCode)}", order.ShipPostalCode),
+                command.CreateParameter($"@{nameof(order.ShipCountry)}", order.ShipCountry)
+            });
+
+            command.CommandText = commandText;
+            command.ExecuteNonQuery();
         }
 
         public IEnumerable<Order> GetOrders()
--- Module4/Northwind/Northwind.Tests/OrderRepositoryTests.cs
+++ Module4/Northwind/Northwind.Tests/OrderRepositoryTests.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Linq;
 using Northwind.DAL;
+using Northwind.DAL.Entities;
 using Xunit;
 
 namespace Northwind.Tests
@@ -26,6 +28,24 @@ namespace Northwind.Tests
             var res = rep.GetOrderById(10253);
 
             Assert.False(res == null);
+        }
+
+        [Fact]
+        public void AddOrder_insert()
+        {
+            var rep = new OrderRepository("System.Data.SqlClient",
+                "Server=EPBYBREW0245;Database=Northwind;Trusted_Connection=True;");
+
+            var newOrder = new Order
+            {
+                CustomerID = "ANTON",
+                EmployeeID = 1,
+                Freight = 10,
+                OrderDate = DateTime.Now
+            };
+            rep.AddOrder(newOrder);
+
+            //Assert.True(res != null);
         }
     }
 }
