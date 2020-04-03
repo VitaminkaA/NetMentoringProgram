db8c8e20b679c53631d00e5263099eab8244d974
Tatsiana Panasiuk <Tatsiana.Panasiuk@thomsonreuters.com>
2020-04-03 20:25:46 +0300

    asfsdfdsfsdf

--- Module7/WGetAnalogue/WGetAnalogue.ConsoleApp/Program.cs
+++ Module7/WGetAnalogue/WGetAnalogue.ConsoleApp/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using WGetAnalogue.Library;
 
 namespace WGetAnalogue.ConsoleApp
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var res = Console.ReadLine();
+
+            MyClass myCass = new MyClass();
+            foreach (var VARIABLE in Encoding.GetEncodings())
+            {
+                Console.WriteLine(VARIABLE.DisplayName); 
+            }
+
+
+
+            var rese = await myCass.GetText(res);
+            Console.WriteLine(rese);
         }
     }
 }
---
+++ Module7/WGetAnalogue/WGetAnalogue.Library/Services/HTTPService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WGetAnalogue.Library.Services
+{
+    public class HTTPService
+    {
+        private readonly HttpClient _client;
+
+        public HTTPService()
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            _client = new HttpClient();
+        }
+
+        public async Task<string> GetSite(string url)
+        {
+            var uri = new Uri(url ?? throw new ArgumentNullException());
+            var res = await _client.GetAsync(uri);
+            byte[] buffer = await res.Content.ReadAsByteArrayAsync();
+
+            Encoding encoding = Encoding.GetEncoding("windows-1251");
+
+            return encoding.GetString(buffer,0,buffer.Length);
+        }
+    }
+}
