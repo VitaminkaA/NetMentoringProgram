b4337dc303fc8a8cea933067468d376b8d3fc764
Tatsiana Panasiuk <Tatsiana.Panasiuk@thomsonreuters.com>
2020-04-22 13:10:24 +0300

    Added a project files.

---
+++ Module9/Caching/Caching.FibonacciNumbers.ConsolApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using Caching.FibonacciNumbers.Library.Services;
+using System;
+
+namespace Caching.FibonacciNumbers.ConsolApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
